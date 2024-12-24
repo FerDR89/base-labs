12568eb15854286285b128c440797931bf2653cf
Fernando de Row <ferdr89dev@gmail.com>
2024-12-24 10:06:44 -0300

    add rateLimit middleware

--- be/src/utils/middlewares.js
+++ be/src/utils/middlewares.js
@@ -1,4 +1,5 @@
 import { decodeToken } from "./jwt.js";
+import pool from "../db.js";
 
 const authenticate = async (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -19,4 +20,48 @@ const authenticate = async (req, res, next) => {
   }
 };
 
-export { authenticate };
+const rateLimit = (resource) => {
+  return async (req, res, next) => {
+    const userId = req.userId;
+    if (!userId) {
+      return res.status(500).json({ error: "User ID missing from request" });
+    }
+
+    try {
+      const client = await pool.connect();
+      const query = `SELECT last_access FROM rate_limits WHERE user_id = $1 AND resource = $2`;
+      const values = [userId, resource];
+      const result = await client.query(query, values);
+
+      const now = new Date();
+
+      if (result.rows.length > 0) {
+        const lastAccess = new Date(result.rows[0].last_access);
+        const timeDifference = (now - lastAccess) / 1000;
+
+        if (timeDifference < 60) {
+          client.release();
+          return res
+            .status(429)
+            .json({ error: "Rate limit exceeded. Try again later." });
+        }
+      }
+
+      const upsertQuery = `
+              INSERT INTO rate_limits (user_id, resource, last_access) 
+              VALUES ($1, $2, $3)
+              ON CONFLICT (user_id, resource) 
+              DO UPDATE SET last_access = $3
+          `;
+
+      await client.query(upsertQuery, [userId, resource, now]);
+      client.release();
+      next();
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  };
+};
+
+export { authenticate, rateLimit };
