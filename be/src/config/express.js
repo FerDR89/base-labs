e175a44fbebdd693d1650a84880d9c93ccc52167
Fernando de Row <ferdr89dev@gmail.com>
2024-12-27 11:16:03 -0300

    chore: add allowed origin domain in cors

--- be/src/config/express.js
+++ be/src/config/express.js
@@ -1,12 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 export const app = express();
 
-// app.use(
-//     cors({
-//       origin: "http://localhost:3031",
-//     })
-//   );
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(express.json());
 
