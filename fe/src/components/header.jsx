4dac1a22df42c5d6b9294c2cd676cacac8d8c764
Fernando de Row <ferdr89dev@gmail.com>
2024-12-28 15:54:54 -0300

    feat: add Header

---
+++ fe/src/components/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="h-[10vh] w-screen max-w-screen-xl  flex items-center">
+      <h2 className="text-3xl ">Base Labs Challenge</h2>
+    </header>
+  );
+};
+
+export default Header;
